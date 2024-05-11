96750b13d184dd57bb24bfff1704d6282ee0ffd8
Udbhav Munipally <udbhav4@gmail.com>
2024-05-11 18:59:44 +0530

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,8 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { NavigateFunction, useLocation } from "react-router-dom";
 
-export const API_V1 =
-  "https://payments-app-api-beryl.vercel.app/api/v1" || process.env.API_V1;
+export const API_V1 = process.env.API_V1;
 
 interface Signup {
   firstName?: string;
